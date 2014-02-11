2683aaac2f08c39225b4c8063130d7e0db2c563e
K Mak <karen.mak@stx.oxon.org>
2014-02-10 21:05:57 -0500

    associations specs passed for Submission model

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -4,6 +4,11 @@ describe Submission do
   describe "validations" do
     it { should validate_presence_of(:link)}
     it { should validate_presence_of(:description)}
-    it{ should validate_presence_of(:user_id)}
+    it { should validate_presence_of(:user_id)}
+  end
+
+  describe "associations" do
+    it { should belong_to :user}
+    it { should have_many :comments}
   end
 end
