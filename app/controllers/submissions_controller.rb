1c6aa49fead0a338d41cf67616bfc98a89408b1b
K Mak <karen.mak@stx.oxon.org>
2014-02-10 21:33:42 -0500

    user sign in specs passed with user authentication

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,5 +1,6 @@
 class SubmissionsController < ApplicationController
 
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
   end
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Sign_in' do
+
+  background do @user = User.create!(email: 'karen@example.com', password: 'password')
+  end
+
+  scenario "User can sign in" do
+    visit new_user_session_path
+    fill_in "Email", with: 'karen@example.com'
+    fill_in "Password", with: 'password'
+    click_on "Sign in"
+    expect(page).to have_content ('karen@example.com')
+  end
+end
